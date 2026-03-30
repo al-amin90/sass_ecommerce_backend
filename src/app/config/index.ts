d0689852bfd6b14220737b59724cba126f67d533
al-amin90 <ijesun30@gmail.com>
2026-03-30 23:47:12 +0600

    feat: db connetion url config

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -6,11 +6,23 @@ dotenv.config({ path: path.join(process.cwd(), ".env") });
 export default {
   node_env: process.env.NODE_ENV,
   port: process.env.PORT,
-  database_url: process.env.DATABASE_URL,
+
   bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
   default_password: process.env.DEFAULT_PASSWORD,
-  jwt_access_token: process.env.JWT_ACCESS_TOKEN,
-  jwt_refresh_token: process.env.JWT_REFRESH_TOKEN,
-  jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN,
-  jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
+
+  jwt: {
+    access_token: process.env.JWT_ACCESS_TOKEN,
+    refresh_token: process.env.JWT_REFRESH_TOKEN,
+    access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN,
+    refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
+  },
+  db: {
+    singleUri: process.env.SINGLE_DB_URL,
+    multiUri: process.env.MULTI_DB_URL,
+    centralUri: process.env.CENTRAL_DB_URL,
+
+    singleProductionUri: process.env.SINGLE_PRODUCTION_DB_URL,
+    multiProductionUri: process.env.MULTI_PRODUCTION_DB_URL,
+    centralProductionUri: process.env.CENTRAL_PRODUCTION_DB_URL,
+  },
 };
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 import app from "./app";
 import config from "./app/config";
+import { Server } from "http";
 
 let server: Server;
 
 async function main() {
-  // await mongoose.connect(config.database_url as string)
+  await mongoose.connect(config.db.singleUri as string);
 
   app.listen(config.port, () => {
     console.log(`Example app listening on port ${config.port}`);
