b3615105c757aed276af2fad780fdebf026e2716
al-amin90 <ijesun30@gmail.com>
2026-04-01 00:29:55 +0600

    feat: single multi api methods db manage remove clean up mathods

--- src/app/config/db.ts
+++ src/app/config/db.ts
@@ -1,7 +1,9 @@
 import EventEmitter from "events";
-import { Connection } from "mongoose";
+import mongoose, { Connection } from "mongoose";
 import config from ".";
 import { ConnectionMetadata, DBConfig, TenancyType } from "../interface/db";
+import AppError from "../errors/AppError";
+import status from "http-status";
 
 class DBManager extends EventEmitter {
   private centralConnection: Connection | null = null;
@@ -46,6 +48,7 @@ class DBManager extends EventEmitter {
         };
   }
 
+  // :::) set pool size and connections
   private getConnectionOptions(poolSize: { min: number; max: number }) {
     return {
       maxPoolSize: poolSize.max,
@@ -57,5 +60,132 @@ class DBManager extends EventEmitter {
       retryReads: true,
       maxIdleTimeMS: this.config.maxIdleTime,
     };
+  }
+
+  // :::) db connections from here
+  async initialize() {
+    if (this.tenancyType === "multi") {
+      await this.initCentralConnection();
+    } else {
+      await this.getSingleConnection();
+    }
+
+    this.startCleanupJob();
+  }
+
+  private async initCentralConnection() {
+    if (this.centralConnection?.readyState === 1) return this.centralConnection;
+
+    const uris = this.getDbUris();
+    const options = this.getConnectionOptions(this.config.poolSize.central);
+    console.log("Connecting to Central DB...");
+
+    this.centralConnection = await mongoose.createConnection(
+      uris.central,
+      options,
+    );
+
+    this.setupConnectionHandlers(this.centralConnection, "CENTRAL_DB");
+    console.log("Central DB connected.....");
+
+    this.emit("centralConnected");
+
+    return this.centralConnection;
+  }
+
+  private async getSingleConnection(): Promise<Connection> {
+    if (this.singleConnection?.readyState === 1) return this.singleConnection;
+
+    const uris = this.getDbUris();
+    const options = this.getConnectionOptions(this.config.poolSize.single);
+
+    console.log("🔄 Connecting to Single Tenant DB...");
+    this.singleConnection = await mongoose
+      .createConnection(uris.single, options)
+      .asPromise();
+
+    this.setupConnectionHandlers(this.singleConnection, "SINGLE_DB");
+    console.log("✅ Single DB connected");
+    this.emit("singleConnected");
+
+    return this.singleConnection;
+  }
+
+  // :::) for every method call this method for connection
+  async getConnection(tenantId?: string): Promise<Connection> {
+    if (this.isShuttingDown)
+      throw new AppError(status.INTERNAL_SERVER_ERROR, "System shutting down");
+
+    if (this.tenancyType === "single") return this.getSingleConnection();
+
+    if (!tenantId)
+      throw new AppError(
+        status.UNPROCESSABLE_ENTITY,
+        "tenantId required in multi-tenant mode",
+      );
+
+    return this.getTenantConnection(tenantId);
+  }
+
+  getCentralConnection(): Connection | null {
+    return this.centralConnection?.readyState === 1
+      ? this.centralConnection
+      : null;
+  }
+
+  // :::) tanent connection managment
+  private async getTenantConnection(tenantId: string): Promise<Connection> {
+    const existing = this.tenantConnections.get(tenantId);
+
+    if (existing) {
+      if (existing.readyState === 1) {
+        this.updateConnectionMetadata(tenantId);
+        return existing;
+      }
+
+      await this.removeConnection(tenantId);
+    }
+
+    if (this.tenantConnections.size > this.config.maxConnections) {
+      await this.remo;
+    }
+  }
+
+  // :::) matedata methods are here
+  private updateConnectionMetadata(tenantId: string): void {
+    const existing = this.connectionMetadata.get(tenantId);
+    this.connectionMetadata.set(tenantId, {
+      lastAccess: Date.now(),
+      accessCount: (existing?.accessCount ?? 0) + 1,
+      createdAt: existing?.createdAt ?? Date.now(),
+    });
+  }
+
+  // :::) cleann up methods are here
+  private async removeConnection(tenantId: string): Promise<void> {
+    const conn = this.tenantConnections.get(tenantId);
+    if (!conn) return;
+    try {
+      await conn.close();
+    } finally {
+      this.tenantConnections.delete(tenantId);
+      this.connectionMetadata.delete(tenantId);
+      console.log(`Removed: ${tenantId} `);
+      this.emit("connectionRemoved", { tenantId });
+    }
+  }
+
+  private async removeOldestIdleConnection(): Promise<void> {
+    let oldestId: string | null = null;
+    let oldestTime: number = Date.now();
+
+    for (const [id, meta] of this.connectionMetadata) {
+      if (meta.lastAccess < oldestTime) {
+        oldestTime = meta.lastAccess;
+        oldestId = id;
+      }
+    }
+
+    if (oldestId) await this.removeConnection(oldestId);
   }
 }
