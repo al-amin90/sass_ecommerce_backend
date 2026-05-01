8549115928966c48c9b5db6c322049a45a918b93
al-amin90 <ijesun30@gmail.com>
2026-05-02 00:48:07 +0600

    build error: auth validator issuse in build

--- src/app/middlewares/validateRequest.ts
+++ src/app/middlewares/validateRequest.ts
@@ -1,9 +1,9 @@
 import { NextFunction, Request, Response } from "express";
 
 import catchAsync from "../utils/catchAsync";
-import { AnyZodObject } from "zod/v3";
+import { ZodSchema } from "zod";
 
-const validateRequest = (schema: AnyZodObject) => {
+const validateRequest = (schema: ZodSchema) => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     await schema.parseAsync({ body: req.body, cookies: req.cookies });
     return next();
